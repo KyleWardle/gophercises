63fdee47a60acbeee83bffbdcfb3be286b3dee13
Kyle Wardle <kylewardle1@live.co.uk>
2022-03-10 12:46:26 +0000

    Added sitemap builder!

--- sitemap-builder/go.mod
+++ sitemap-builder/go.mod
@@ -2,7 +2,8 @@ module sitemap-builder
 
 go 1.17
 
-require (
-	github.com/KyleWardle/gophercises/html-link-parser v0.0.0-20220310104057-b51db8e68298
-	github.com/davecgh/go-spew v1.1.1
-)
+require github.com/KyleWardle/gophercises/html-link-parser v0.0.0-20220310110546-c59adf40b196
+
+require github.com/davecgh/go-spew v1.1.1
+
+require golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
--- sitemap-builder/main.go
+++ sitemap-builder/main.go
@@ -1,7 +1,81 @@
 package main
 
+import (
+	"flag"
+	"github.com/KyleWardle/gophercises/html-link-parser/parser"
+	"github.com/davecgh/go-spew/spew"
+	"net/http"
+	"strings"
+)
+
 func main() {
-	//resp, _ := http.Get("calhoun.io")
-	//links := parser.ParseLinks(	resp.Body)
-	//spew.Dump(links)
+	url := flag.String("url", "https://calhoun.io", "")
+	flag.Parse()
+
+	sitemap := map[string]bool{}
+	sitemap = checkSite(*url, "/", sitemap)
+	spew.Dump(sitemap)
+}
+
+func checkSite(baseUrl string, href string, sitemap map[string]bool) map[string]bool {
+	spew.Dump("Running " + baseUrl + href)
+	resp, err := http.Get(baseUrl + href)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	links := parser.ParseLinks(resp.Body)
+
+	for _, link := range links {
+		//spew.Dump("Checking " + link.Href)
+
+		localLink := getLocalDomain(link.Href, baseUrl)
+		if localLink == "" {
+			continue
+		}
+
+		if sitemap[baseUrl+localLink] {
+			continue
+		}
+
+		sitemap[baseUrl+localLink] = true
+		sitemap = checkSite(baseUrl, localLink, sitemap)
+	}
+
+	return sitemap
+}
+
+func getLocalDomain(newLink string, originalLink string) string {
+	if newLink[len(newLink)-1:] == "/" {
+		newLink = newLink[0:(len(newLink) - 1)]
+	}
+
+	if len(newLink) == 0 {
+		return ""
+	}
+
+	if newLink[0:1] == "/" {
+		return newLink
+	}
+
+	newLink = stripAllLinkStuff(newLink)
+	originalLink = stripAllLinkStuff(originalLink)
+
+	if len(newLink) < len(originalLink) {
+		return ""
+	}
+
+	if newLink[0:len(originalLink)] == originalLink {
+		return newLink[len(originalLink):]
+	}
+
+	return ""
+}
+
+func stripAllLinkStuff(link string) string {
+	link = strings.ReplaceAll(link, "www.", "")
+	link = strings.ReplaceAll(link, "https://", "")
+	link = strings.ReplaceAll(link, "http://", "")
+	return link
 }
